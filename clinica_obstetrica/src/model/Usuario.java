071c84e46d4c4955f12314b2f37f17147d679f2d
Ana Beatriz Almeida da Silva <abeatrizpb@gmail.com>
2025-07-03 12:08:27 -0300

    Criação dos métodos  da super classe Usuario,  getters e setters

--- clinica_obstetrica/src/model/Usuario.java
+++ clinica_obstetrica/src/model/Usuario.java
@@ -10,4 +10,48 @@ public class Usuario {
         this.senha = senha;
         this.email = email;
     }
+
+    public boolean autenticar(String email, String senha) {
+        return this.email.equals(email) && this.senha.equals(senha);
+    }
+
+    public Usuario atualizarCredenciais(String novoNomeUsuario, String novaSenha) {
+        this.nomeUsuario = novoNomeUsuario;
+        this.senha = novaSenha;
+        return this;
+    }
+
+    public String getTipoUsuario() {
+        if (email.endsWith("@medico")) {
+            return "Medico";
+        } else if (email.endsWith("@paciente")) {
+            return "Paciente";
+        } else {
+            throw new IllegalArgumentException("Email deve terminar com @medico ou @paciente");
+        }
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
