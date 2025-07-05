cc545bd1d5e2a73d9d4bda830338f456eb84ebaa
Shamyra Carvalho <147446284+shamyracarvalhoo@users.noreply.github.com>
2025-07-05 17:42:44 -0300

    criação da classe medico

---
+++ clinica_obstetrica/src/model/Medico.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Medico {
+    private String crm;
+    private String especialidade;
+
+    public Medico(String crm, String especialidade) {
+        super(nomeUsuario, senha, email);
+        this.crm = crm;
+        this.especialidade = especialidade;
+    }
+}
