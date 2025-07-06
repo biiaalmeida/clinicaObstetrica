d187957c330031aaabdc88cfee9102f2b522a26d
Ana Beatriz Almeida da Silva <abeatrizpb@gmail.com>
2025-07-06 17:47:17 -0300

    reestruturação nas pastas

--- src/model/Paciente.java
+++ src/model/Paciente.java
@@ -1,5 +1,32 @@
 package model;
 
-public class Paciente {
 
+public class Paciente extendes Usuario{
+    private String cpf;
+    private int idade;
+    private String telefoneContato;
+    private String tipoPlanoSaude;
+    private String tipoSanguineo;
+    private String alergias;
+    private int numGestacoesAnteriores;
+    private String vacinas;
+    private Float peso;
+    private String condicoesPreEx;
+
+    public Paciente(String nome, String email, String senha, String cpf, int idade, String telefoneContato,String tipoPlanoSaude, String tipoSanguineo, 
+    String alergias, int numGestacoesAnteriores, String vacinas, Float peso, String condicoesPreEx) {
+
+        super(nome, email, senha);
+        this.cpf = cpf;
+        this.idade = idade;
+        this.telefoneContato = telefoneContato;
+        this.tipoPlanoSaude = tipoPlanoSaude;
+        this.tipoSanguineo = tipoSanguineo;
+        this.alergias = alergias;
+        this.numGestacoesAnteriores = numGestacoesAnteriores;
+        this.vacinas = vacinas;
+        this.peso = peso;
+        this.condicoesPreEx = condicoesPreEx;
+    }
+ 
 }
