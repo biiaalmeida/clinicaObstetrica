852bf3e1a9410dc981fcb6166868e00b2ce5b70c
Ana Beatriz Almeida da Silva <abeatrizpb@gmail.com>
2025-07-09 14:02:16 -0300

    Atualiza classe Consulta com melhorias

--- src/model/Consulta.java
+++ src/model/Consulta.java
@@ -1,20 +1,115 @@
-    package model;
+package model;
 
-    public class Consulta {
-        private Int codigoConsulta;
-        private Date dataConsulta;
-        private Date dataPrevista;
-        private Date dataUltimaMenstruacao;
-        private String tipoParto;
-        private String qtdSemanas;
+import java.time.LocalDate;
 
-        public Consulta(Int codigoConsulta, Date dataConsulta, Date dataPrevista, Date dataUltimaMenstruacao, String tipoParto, String qtdSemanas) {
-            super(nomeUsuario, senha, email);
-            this.codigoConsulta = codigoConsulta;
-            this.dataConsulta = dataConsulta;
-            this.dataPrevista = dataPrevista;
-            this.dataUltimaMenstruacao = dataUltimaMenstruacao;
-            this.tipoParto = tipoParto;
-            this.qtdSemanas = qtdSemanas;
-        }
+public class Consulta {
+    private int codigoConsulta;
+    private LocalDate dataConsulta;
+    private LocalDate dataPrevistaParto;
+    private LocalDate dataUltimaMenstruacao;
+    private String tipoParto;
+    private String qtdSemanas;
+
+    private Paciente paciente;
+    private Medico medico;
+
+    public Consulta(int codigoConsulta, LocalDate dataConsulta, LocalDate dataPrevistaParto,
+            LocalDate dataUltimaMenstruacao, String tipoParto, String qtdSemanas,
+            Paciente paciente, Medico medico) {
+        this.codigoConsulta = codigoConsulta;
+        this.dataConsulta = dataConsulta;
+        this.dataPrevistaParto = dataPrevistaParto;
+        this.dataUltimaMenstruacao = dataUltimaMenstruacao;
+        this.tipoParto = tipoParto;
+        this.qtdSemanas = qtdSemanas;
+        this.paciente = paciente;
+        this.medico = medico;
     }
+
+    // Construtor vazio
+    public Consulta() {
+    }
+
+    // Getters e Setters
+    public int getCodigoConsulta() {
+        return codigoConsulta;
+    }
+
+    public void setCodigoConsulta(int codigoConsulta) {
+        this.codigoConsulta = codigoConsulta;
+    }
+
+    public LocalDate getDataConsulta() {
+        return dataConsulta;
+    }
+
+    public void setDataConsulta(LocalDate dataConsulta) {
+        this.dataConsulta = dataConsulta;
+    }
+
+    public LocalDate getDataPrevistaParto() {
+        return dataPrevistaParto;
+    }
+
+    public void setDataPrevistaParto(LocalDate dataPrevistaParto) {
+        this.dataPrevistaParto = dataPrevistaParto;
+    }
+
+    public LocalDate getDataUltimaMenstruacao() {
+        return dataUltimaMenstruacao;
+    }
+
+    public void setDataUltimaMenstruacao(LocalDate dataUltimaMenstruacao) {
+        this.dataUltimaMenstruacao = dataUltimaMenstruacao;
+    }
+
+    public String getTipoParto() {
+        return tipoParto;
+    }
+
+    public void setTipoParto(String tipoParto) {
+        this.tipoParto = tipoParto;
+    }
+
+    public String getQtdSemanas() {
+        return qtdSemanas;
+    }
+
+    public void setQtdSemanas(String qtdSemanas) {
+        this.qtdSemanas = qtdSemanas;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public void cadastrarConsulta() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Consulta{" +
+                "codigoConsulta=" + codigoConsulta +
+                ", dataConsulta=" + dataConsulta +
+                ", dataPrevistaParto=" + dataPrevistaParto +
+                ", dataUltimaMenstruacao=" + dataUltimaMenstruacao +
+                ", tipoParto='" + tipoParto + '\'' +
+                ", qtdSemanas='" + qtdSemanas + '\'' +
+                ", paciente=" + (paciente != null ? paciente.getCpf() : "N/A") +
+                ", medico=" + (medico != null ? medico.getCrm() : "N/A") +
+                '}';
+    }
+}
