c42e93d075a1b4d1fada4eec78963468c0d940ef
Shamyra Carvalho <147446284+shamyracarvalhoo@users.noreply.github.com>
2025-07-09 16:50:19 -0300

    adição de get e set na classe médico

--- src/model/Medico.java
+++ src/model/Medico.java
@@ -4,9 +4,26 @@ public class Medico {
     private String crm;
     private String especialidade;
 
-    public Medico(String crm, String especialidade) {
+    public Medico(String nomeUsuario, String email, String senha,String crm, String especialidade) {
         super(nomeUsuario, senha, email);
         this.crm = crm;
         this.especialidade = especialidade;
     }
 }
+    public String getcrm() {
+        return crm;
+    }
+
+    public void setcrm(String crm) {
+        this.crm = crm;
+    }
+
+    public String getespecialidade() {
+        return especialidade;
+    }
+
+    public void setespecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    
