0a97dd62bdeb9b8802d1d7a681880d43639f2f49
Shamyra Carvalho <147446284+shamyracarvalhoo@users.noreply.github.com>
2025-07-14 14:23:03 -0300

    conclusão da classe medico

--- src/model/Medico.java
+++ src/model/Medico.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Medico {
     private String crm;
     private String especialidade;
@@ -10,19 +14,19 @@ public class Medico {
         this.especialidade = especialidade;
     }
 }
-    public String getcrm() {
+    public String getCrm() {
         return crm;
     }
 
-    public void setcrm(String crm) {
+    public void setCrm(String crm) {
         this.crm = crm;
     }
 
-    public String getespecialidade() {
+    public String getEspecialidade() {
         return especialidade;
     }
 
-    public void setespecialidade(String especialidade) {
+    public void setEspecialidade(String especialidade) {
         this.especialidade = especialidade;
     }
 
@@ -32,6 +36,73 @@ public class Medico {
                 "crm='" + crm + '\'' +
                 ", especialidade='" + especialidade + '\'' +
                 '}';
-    }
+    }   
+public boolean cadastrarMedico (){
 
+    String sql = "INSERT INTO medico (crm, especialidade) VALUES (?, ?)";
+
+    try (Connection connection = ConexaoPostegres.getConnection();
+        PreparedStatement novoMedico = connection.prepareStatement(sql)) {
+
+            novoMedico.setString(1, this.crm);
+            novoMedico.setString(2, this.especialidade);
+            
+            novoMedico.executeUpdate();
+            System.out.println("Médico cadastrado com sucesso!");
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Erro ao cadastrar médico: " + e.getMessage());
+            return false;
+        }
+}
+
+public boolean editarMedico(){
+    String sql = "UPDATE medico SET especialidade = ? WHERE crm = ?";
+
+    try (Connection connection = ConexaoPostegres.getConnection();
+        PreparedStatement editarMedico = connection.prepareStatement(sql)) {
+
+            editarMedico.setString(1, this.especialidade);
+            editarMedico.setString(2, this.crm);
+            
+            editarMedico.executeUpdate();
+            System.out.println("Médico editado com sucesso!");
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Erro ao editar médico: " + e.getMessage());
+            return false;
+        }
+}
+
+public Medico buscarMedico(String crm){
+    String sql = "SELECT * FROM medico WHERE crm = ?";
+    
+    try (Connection connection = ConexaoPostegres.getConnection();
+
+         PreparedStatement buscarMedico = connection.prepareStatement(sql)) {
+        
+        buscarMedico.setString(1, crm);
+        ResultSet resultSet = buscarMedico.executeQuery();
+        
+        if (resultSet.next()) {
+            String especialidade = resultSet.getString("especialidade");
+            return new Medico(crm, especialidade);
+        } else {
+            System.out.println("Médico não encontrado.");
+            return null;
+        }
+    } catch (SQLException e) {
+        System.out.println("Erro ao buscar médico: " + e.getMessage());
+        return null;
+    }
+}
+
+public vois imprimirMedico(String crm){
+    Medico medico = buscarMedico(crm);
+    if (medico != null) {
+        System.out.println(medico.toString());
+    } else {
+        System.out.println("Médico não encontrado.");
+    }
+}
     
